fdbea825ddc32ade069b93bae77d314381e160e5
Franck Nsabimana <sgenlecroyant@gmail.com>
2026-04-13 12:21:53 +0200

    config discovery-server

---
+++ src/main/java/com/salesmanagement/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.salesmanagement.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
